a2a949b0f95acc2bb7d22832155d6c9283b66c98
Charlie Lee <charle0911@gmail.com>
2020-09-05 20:38:08 +0800

    Add count down latch

--- src/concurrency/merge/CountDownLatchExample.java
+++ src/concurrency/merge/CountDownLatchExample.java
@@ -1,9 +1,74 @@
 package concurrency.merge;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+class TaskSet {
+    public void taskOne() {
+        try {
+            String currentThread = Thread.currentThread().getName();
+            System.out.println("T1 task start in " + currentThread);
+            // do something that need long time
+            Thread.sleep(4000);
+            System.out.println("T1 task finish");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void taskTwo() {
+        {
+            try {
+                String currentThread = Thread.currentThread().getName();
+                System.out.println("T2 task start in " + currentThread);
+                // do something that need long time
+                Thread.sleep(2000);
+                System.out.println("T2 task finish");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
 public class CountDownLatchExample {
 
+    public Executor executor = Executors.newFixedThreadPool(2);
 
-    public static void main(String[] args) {
+    private CountDownLatch countDownLatch;
 
+    public CountDownLatchExample(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+    }
+
+    public void fork(Runnable task) {
+        executor.execute(() -> {
+            task.run();
+            countDownLatch.countDown();
+        });
+
+    }
+
+    public void merge() throws InterruptedException {
+        countDownLatch.await();
+        System.out.println("T1 task and T2 task are all already");
+        System.out.println("Do the main thread task");
+    }
+
+    public void solve(TaskSet taskSet) throws InterruptedException {
+        System.out.println("Start t1 and t2 task");
+        fork(taskSet::taskOne);
+        fork(taskSet::taskTwo);
+        merge();
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            CountDownLatchExample countDownLatchExample = new CountDownLatchExample(new CountDownLatch(2));
+            countDownLatchExample.solve(new TaskSet());
+        }
     }
 }
